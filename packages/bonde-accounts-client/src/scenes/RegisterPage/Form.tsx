358cdc315c200f44deb0593b023b92f9646cd973
Igor Santos <igr.exe@gmail.com>
2020-04-02 03:34:10 -0300

    fix(client): add error on register with invalid invite

--- packages/bonde-accounts-client/src/scenes/RegisterPage/Form.tsx
+++ packages/bonde-accounts-client/src/scenes/RegisterPage/Form.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import gql from 'graphql-tag'
+import styled from 'styled-components';
 import { useLocation, Link } from 'react-router-dom';
 import { useSession, useMutation } from 'bonde-core-tools';
-import { Button, ConnectedForm, InputField, Header, Link as LinkStyled } from 'bonde-components';
+import { Button, ConnectedForm, InputField, Header, Link as LinkStyled, Hint } from 'bonde-components';
 import { composeValidators, required, min } from '../../validations';
 import Container from '../../components/Container';
 
@@ -15,9 +16,18 @@ const registerUserMutation = gql`
   }
 `
 
+const Styles = styled.div`
+  & > span {
+    display: block;
+    margin-bottom: 15px;
+    text-align: right;
+  }
+`
+
 const RegisterForm = ({ to }: any) => {
   const { search } = useLocation();
   const { login } = useSession();
+  const [error, setError] = useState(undefined);
   const [registerUser] = useMutation(registerUserMutation);
 
   const urlParams = new URLSearchParams(search);
@@ -31,18 +41,24 @@ const RegisterForm = ({ to }: any) => {
         initialValues={{ input: { email, invitation_code: code } }}
         onSubmit={async (values: any) => {
           try {
-            const { data } = await registerUser({ variables: values });
-            
-            login(data.register).then(() => {
-              window.location.href = to;
-            });
+            const { data } = await registerUser({ variables: values })
+            login(data.register)
+              .then(() => {
+                window.location.href = to;
+              })
           } catch (err) {
+            if (err && err.message && err.message.indexOf('invalid_invitation_code') !== -1) {
+              setError('Seu convite não é válido, peça um novo convite para a comunidade')
+              console.log('err', err)
+            }
             console.log('RegisterFailed', err)
           }
         }}
       >
         {({ submitting }) => (
-          <>
+          <Styles>
+            {error && <Hint color='error'>{error}</Hint>}
+            <Hint color='error'>Seu convite não é válido, peça um novo convite para a comunidade</Hint>
             <Container column>
               <InputField
                 name='input.first_name'
@@ -77,7 +93,7 @@ const RegisterForm = ({ to }: any) => {
               <LinkStyled to='/auth/login' component={Link}>Já tenho conta</LinkStyled>
               <Button type='submit' disabled={submitting}>Partiu</Button>
             </Container>
-          </>
+          </Styles>
         )}
       </ConnectedForm>
     </>
