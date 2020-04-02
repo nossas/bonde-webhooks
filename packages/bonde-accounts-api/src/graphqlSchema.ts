798dea7071ffdc03226d996583865f469ce55e15
Igor Santos <igr.exe@gmail.com>
2020-04-02 13:58:04 -0300

    fix(api): remove accept_invite mutation

--- packages/bonde-accounts-api/src/graphqlSchema.ts
+++ packages/bonde-accounts-api/src/graphqlSchema.ts
@@ -1,5 +1,4 @@
 import {
-  accept_invite,
   authenticate,
   register,
   reset_password_request,
@@ -17,14 +16,12 @@ export const typeDefs = `
   }
   type DecodedToken { id: Int!, expired_at: Int!, iat: Int! }
   type JWT { valid: Boolean!, token: String, first_name: String }
-  type AcceptInvite { email: String!, invitation_code: String!, is_new_user: Boolean! }
 
 	type Query {
     authenticate(email: String!, password: String!): JWT
     reset_password_verify(token: String!): DecodedToken
   }
   type Mutation {
-    accept_invite(email: String!, invitation_code: String!): AcceptInvite
     authenticate(email: String!, password: String!): JWT
     register(input: RegisterInput!): JWT
     reset_password_change(password: String!, token: String!): JWT
@@ -39,7 +36,6 @@ export const resolvers = {
     reset_password_verify
   },
   Mutation: {
-    accept_invite,
     authenticate,
     register,
     reset_password_request,
