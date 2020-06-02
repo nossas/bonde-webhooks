52c52222f215e79f85d8de2453d3091bcc6afc7c
Igor Santos <igr.exe@gmail.com>
2020-06-02 17:01:39 -0300

    feat(webhook): add env to mark users admin

--- packages/bonde-accounts-webhook/src/Server.ts
+++ packages/bonde-accounts-webhook/src/Server.ts
@@ -1,8 +1,8 @@
 import Express from 'express'
 import dotenv from 'dotenv'
 import debug, { Debugger } from 'debug'
-import jwt from 'jsonwebtoken'
 import invitations from './handles/invitations';
+import hasura from './handles/hasura';
 
 dotenv.config()
 
@@ -19,32 +19,11 @@ class Server {
     res.status(200).json({ status: 'running' })
   }
 
-  private hasura = async (req: any, res: any) => {
-    const authorization = req.get('Authorization')
-    const token = authorization ? authorization.replace('Bearer ', '') : null
-    jwt.verify(token, process.env.JWT_SECRET, (err: any, decoded: any) => {
-      if (decoded) {
-        const hasuraVariables = {
-          'X-Hasura-User-Id': String(decoded.user_id),
-          'X-Hasura-Role': decoded.role
-        }
-        this.dbg(`Logged ${decoded.role} user.`)
-        res.status(200).json(hasuraVariables)
-      } else if (err.message === 'jwt must be provided') {
-        this.dbg('Logged anonymous user.')
-        res.status(200).json({ 'X-Hasura-Role': 'anonymous' })
-      } else {
-        this.dbg('Unauthorized jwt token.')
-        res.status(401).json('Unauthorized')
-      }
-    })
-  }
-
   start = () => {
     const { PORT, HOST } = process.env
     this.server
       .get('/', this.health.bind(this))
-      .get('/hasura', this.hasura.bind(this))
+      .get('/hasura', hasura)
       .post('/invitations', invitations)
       .listen(Number(PORT), '0.0.0.0', () => {
         this.dbg(`Webhook Auth Server listen on ${HOST}:${PORT}`)
---
+++ packages/bonde-accounts-webhook/src/handles/hasura.ts
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+
+const admins: string[] = (process.env.ADMINS || '').split(',');
+
+const hasura = async (req: any, res: any) => {
+  const authorization = req.get('Authorization');
+  const token = authorization ? authorization.replace('Bearer ', '') : null;
+
+  jwt.verify(token, process.env.JWT_SECRET, (err: any, decoded: any) => {
+    if (decoded) {
+      const hasuraVariables = {
+        'X-Hasura-User-Id': String(decoded.user_id),
+        'X-Hasura-Role': admins.filter((id: string) => id === String(decoded.user_id)).length > 0 ? 'admin' : 'user'
+      };
+
+      return res.status(200).json(hasuraVariables);
+    } else if (err.message === 'jwt must be provided') {
+      return res.status(200).json({ 'X-Hasura-Role': 'anonymous' });
+    };
+
+    return res.status(401).json('Unauthorized');
+  });
+};
+
+export default hasura;
+
+
+/**
+ * Users Roles (Bonde)
+ * - User (Permissions related to the context of a group)
+ * - Admin (Has access to all information, used for developers that are part of Nossas.org)
+ * 
+ * Group Users Roles (Community)
+ * - Mobilizer (Restricted access to information about the community and its modules) (2)
+ * - Owner (Full access to community information and modules) (1)
+ * 
+ */
