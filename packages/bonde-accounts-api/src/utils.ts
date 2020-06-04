ccd1cdb60f0f5456ab066c8a2969ccb6a4d9d60c
Igor Santos <igr.exe@gmail.com>
2020-06-04 17:30:30 -0300

    fix(api): decode token on api-v2 not accept boolean

--- packages/bonde-accounts-api/src/utils.ts
+++ packages/bonde-accounts-api/src/utils.ts
@@ -8,7 +8,7 @@ export const generateJWT = (user: any) => {
     sub: 'postgraphql',
     role: user.admin ? 'admin' : 'common_user',
     user_id: user.id,
-    is_admin: user.is_admin
+    is_admin: user.is_admin ? 1 : 0
   }
   const options = { audience: 'postgraphile' }
   
