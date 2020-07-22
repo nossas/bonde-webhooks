8fb1767fd809773b5d29fb631be5d1925fb7bb93
Igor Santos <igr.exe@gmail.com>
2020-07-22 15:47:00 -0300

    chore(api): update apollo-server-express to apollo-server

--- packages/bonde-accounts-api/src/index.ts
+++ packages/bonde-accounts-api/src/index.ts
@@ -1,4 +1,14 @@
-import Server from './Server'
+import debug, { Debugger } from 'debug'
+import { ApolloServer } from 'apollo-server'
+import { typeDefs, resolvers } from './graphqlSchema'
 
-const app = new Server()
-app.start()
+const dbg: Debugger = debug(process.env.DEBUG);
+
+// Configure graphql schema
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  dbg(`API Auth Server listen on ${url}`);
+});
+
+
